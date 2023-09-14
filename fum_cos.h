290d8476b5500379780385861df5eede8a556353
coxxy666 <ezeiyokecosmas@gmail.com>
2023-09-14 02:02:22 -0700

    main.h

---
+++ fum_cos.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <unistd.h>
+#include <studio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <signal.h>
+#include <error.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stddef.h>
+
+
+
+
+
+
+
+
+#endif
